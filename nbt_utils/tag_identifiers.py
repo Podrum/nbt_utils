26561d48146264e6624558d346623cb497037818
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-10-27 03:21:16 +0300

    Update tag_identifiers.py

--- nbt_utils/tag_identifiers.py
+++ nbt_utils/tag_identifiers.py
@@ -4,7 +4,9 @@ r"""
  | |_) / _ \ / _` | '__| | | | '_ ` _ \
  |  __/ (_) | (_| | |  | |_| | | | | | |
  |_|   \___/ \__,_|_|   \__,_|_| |_| |_|
+ 
  Copyright 2021 Podrum Team.
+ 
  This file is licensed under the GPL v2.0 license.
  The license file is located in the root directory
  of the source code. If not you may not use this file.
