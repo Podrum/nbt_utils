65b82540fbefad1565b418c6cc704b686e272c2b
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-10-30 22:10:57 +0300

    Update test.py

--- test.py
+++ test.py
@@ -3,8 +3,8 @@ from nbt_utils.tag.compound_tag import CompoundTag
 import os
 
 file_dir: str = os.path.abspath(os.path.dirname(__file__))
-file: object = open(file_dir + "/hello_world.nbt", "rb")
-stream: object = nbt_be_binary_stream(file.read())
-tag: object = CompoundTag()
+file = open(file_dir + "/hello_world.nbt", "rb")
+stream: NbtBeBinaryStream = NbtBeBinaryStream(file.read())
+tag: CompoundTag = CompoundTag()
 tag.read(stream)
 print(tag.get_tag("hello world").get_tag("name").value)
