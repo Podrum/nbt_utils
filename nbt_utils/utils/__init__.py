0439fd5d26d47df058063eae5bba3fb6e6296f73
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-10-30 22:09:50 +0300

    Update __init__.py

--- nbt_utils/utils/__init__.py
+++ nbt_utils/utils/__init__.py
@@ -13,13 +13,13 @@ r"""
 """
 
 from nbt_utils.utils.nbt import Nbt
-from nbt_utils.utils.nbt_be_binary_stream import nbt_be_binary_stream
-from nbt_utils.utils.nbt_le_binary_stream import nbt_le_binary_stream
-from nbt_utils.utils.nbt_net_le_binary_stream import nbt_net_le_binary_stream
+from nbt_utils.utils.nbt_be_binary_stream import NbtBeBinaryStream
+from nbt_utils.utils.nbt_le_binary_stream import NbtLeBinaryStream
+from nbt_utils.utils.nbt_net_le_binary_stream import NbtNetLeBinaryStream
 
 __all__ = (
     "Nbt",
-    "nbt_be_binary_stream",
-    "nbt_le_binary_stream",
-    "nbt_net_le_binary_stream"
+    "NbtBeBinaryStream",
+    "NbtLeBinaryStream",
+    "NbtNetLeBinaryStream"
 )
